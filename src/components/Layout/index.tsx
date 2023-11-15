112d4faf8c55a6ec9507cce371479a554305fa0c
Antonio Guzman <aguzmanv09@gmail.com>
2023-11-15 06:37:56 -0600

    Add Layout component to App route

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
+
+export default Layout;
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Layout>
   )
 }
 
