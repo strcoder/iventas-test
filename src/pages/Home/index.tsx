f32671de280945607b9b074325d38b2485c9fa24
Antonio Guzman <aguzmanv09@gmail.com>
2023-11-17 17:45:11 -0600

    Add responsive layout and toggle functionality to Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,37 @@
+import cx from 'classnames';
+import { useState } from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Header from '../../components/header/Header';
+import Aside from '../../components/aside/Aside';
+import ChatContainer from '../../components/chat/ChatContainer';
+import './styles.css';
+
 const Home = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showAside, setShowAside] = useState(false);
+
+  const homeClassess = cx({
+    'Home': true,
+    'Home--showSidebar': showSidebar,
+    'Home--showAside': showAside,
+  });
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={homeClassess}>
+      <Header />
+      <Sidebar />
+      <ChatContainer
+        isActive={showSidebar || showAside}
+        onSearch={() => setShowSidebar(prevState => !prevState)}
+        onDots={() => setShowAside(prevState => !prevState)}
+        onDesactive={() => {
+          setShowSidebar(false);
+          setShowAside(false);
+        }}
+      />
+      <Aside />
     </div>
   );
-}
+};
 
 export default Home;
